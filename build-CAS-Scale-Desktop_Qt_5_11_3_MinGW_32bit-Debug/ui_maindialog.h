fb894dfe937ab3e1289e67987161426c6b67e409
Arashayrang <arashayrang@outlook.com>
2021-08-22 23:40:52 +1000

    Finished

---
+++ build-CAS-Scale-Desktop_Qt_5_11_3_MinGW_32bit-Debug/ui_maindialog.h
@@ -0,0 +1,46 @@
+/********************************************************************************
+** Form generated from reading UI file 'maindialog.ui'
+**
+** Created by: Qt User Interface Compiler version 5.11.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINDIALOG_H
+#define UI_MAINDIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainDialog
+{
+public:
+
+    void setupUi(QDialog *MainDialog)
+    {
+        if (MainDialog->objectName().isEmpty())
+            MainDialog->setObjectName(QStringLiteral("MainDialog"));
+        MainDialog->resize(400, 300);
+
+        retranslateUi(MainDialog);
+
+        QMetaObject::connectSlotsByName(MainDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *MainDialog)
+    {
+        MainDialog->setWindowTitle(QApplication::translate("MainDialog", "MainDialog", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainDialog: public Ui_MainDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINDIALOG_H
